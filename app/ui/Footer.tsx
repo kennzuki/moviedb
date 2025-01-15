e6e23d0c65722635a6a874d4fcf76839e288320f
kennzuki <kingsyken@gmail.com>
2025-01-15 17:04:59 +0300

    footer component

--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 export const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
-    <div>Footer</div>
-  )
-}
+    <div className='bg-[#201F31] flex justify-center place-items-center px-12 py-12'>
+      <p className='text-xl font-medium text-gray-100'> {`Welcome to © Kenki media All rights reserved ${currentYear}.`}</p>
+      
+    </div>
+  );
+};
