fd4862cfdcebcede7b721cdc27e598b246c53ad4
kennzuki <kingsyken@gmail.com>
2025-01-15 16:49:57 +0300

    nav component initial for large screens

--- app/ui/Nav.tsx
+++ app/ui/Nav.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import { PowerIcon } from '@heroicons/react/24/solid'
+import SearchInput from './SearchInput'
+
 
 export const Nav = () => {
   return (
-    <div>
+    <div className='bg-[#201F31] flex justify-between place-items-center p-4'>
       <h1 className="text-3xl uppercase font-bold ">Kenki</h1>
-      <ul className="flex justify-between">
+      <ul className="flex justify-between gap-4">
         <li>Home</li>
         <li>About</li>
         <li>Contact</li>
+        <SearchInput/>
       </ul>
       <div className="">
-      <button className='flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3'>
+      <button className='flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 text-black'>
             <PowerIcon className='w-6' />
             <div className='hidden md:block'>Sign In</div>
           </button>
